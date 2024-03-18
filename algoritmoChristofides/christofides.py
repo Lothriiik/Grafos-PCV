85a32ac27b23fc16d1f7ae135841aa308ba65d11
Uanderson Henrique <uanderson_pb@hotmail.com.br>
2024-03-18 20:11:26 -0300

    Add files via upload

---
+++ algoritmoChristofides/christofides.py
@@ -0,0 +1,49 @@
+import itertools
+
+import numpy as np
+import networkx as nx
+
+from networkx.algorithms.matching import max_weight_matching
+from networkx.algorithms.euler import eulerian_circuit
+
+from utils import minimal_spanning_tree
+
+
+def christofides_tsp(graph, starting_node=0):
+    
+    mst = minimal_spanning_tree(graph, 'Prim', starting_node=0)
+    odd_degree_nodes = list(_get_odd_degree_vertices(mst))
+    odd_degree_nodes_ix = np.ix_(odd_degree_nodes, odd_degree_nodes)
+    nx_graph = nx.from_numpy_array(-1 * graph[odd_degree_nodes_ix])
+    matching = max_weight_matching(nx_graph, maxcardinality=True)
+    euler_multigraph = nx.MultiGraph(mst)
+    for edge in matching:
+        euler_multigraph.add_edge(odd_degree_nodes[edge[0]], odd_degree_nodes[edge[1]],
+                                  weight=graph[odd_degree_nodes[edge[0]]][odd_degree_nodes[edge[1]]])
+    euler_tour = list(eulerian_circuit(euler_multigraph, source=starting_node))
+    path = list(itertools.chain.from_iterable(euler_tour))
+    return _remove_repeated_vertices(path, starting_node)[:-1]
+
+
+def _get_odd_degree_vertices(graph):
+
+    odd_degree_vertices = set()
+    for index, row in enumerate(graph):
+        if len(np.nonzero(row)[0]) % 2 != 0:
+            odd_degree_vertices.add(index)
+    return odd_degree_vertices
+
+
+def _remove_repeated_vertices(path, starting_node):
+    path = list(dict.fromkeys(path).keys())
+    path.append(starting_node)
+    return path
+
+def tour_cost(adj_matrix, tour):
+    cost = 0
+    for i in range(len(tour)):
+        u = tour[i]
+        v = tour[(i + 1) % len(tour)]
+        cost += adj_matrix[u][v]
+    return cost
+
